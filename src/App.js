dfc42c1f7e3ba29a90375962f9b3abd5d92f369d
Yekutiel Katanov <yekutielkatanov@gmail.com>
2024-10-01 00:07:02 +0300

    content navbar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,20 @@ function App() {
                 </div>
                 <text className='header_signout'>Sign out</text>
             </header>
+            <div className='content'>
+                <div className='content_navigate'>
+                    <div className='content_navigate_create'><div>Create</div></div>
+                    <div className='content_navigate_active'><div>Active</div></div>
+                    <div className='content_navigate_contacts'><div>Contacts</div></div>
+                    <div className='content_navigate_group'><div>Group</div></div>
+                    <div className='content_navigate_students'><div>Students</div></div>
+                    <div className='content_navigate_lecturers'><div>Lecturers</div></div>
+                </div>
+                <div className='content_data'>
+
+                </div>
+
+            </div>
             
         </div>
     );
